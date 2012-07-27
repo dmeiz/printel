c1509063b2d869d811abfdcd5ed8b302236479dd
Dan Hensgen <dan@methodhead.com>
2012-07-26 22:29:40 -0400

    Show drawings, add drawing, edit drawing

--- lib/printel.rb
+++ lib/printel.rb
@@ -3,18 +3,20 @@ require 'rails_erd/diagram'
 require 'sequel'
 require 'sqlite3'
 require 'sinatra/base'
+require 'sinatra/reloader'
 
 DB = Sequel.connect('sqlite:///Users/dhensgen/src/printel/sample/printel.db')
 
 module Printel
   class Model < Sequel::Model
-    set_primary_key :id
   end
 
   class Relationship < Sequel::Model
-    set_primary_key :id
     many_to_one :source, :class => Model
     many_to_one :destination, :class => Model
+  end
+
+  class Drawing < Sequel::Model
   end
 
   class Diagram < RailsERD::Diagram
@@ -28,8 +30,22 @@ module Printel
   end 
 
   class Server < Sinatra::Base
+    register Sinatra::Reloader
+
     get '/' do
-      'Hello from Printel!'
+      @drawings = Drawing.all
+      erb :index
+    end
+
+    post '/drawings' do
+      Drawing.create(:name => params[:name])
+      redirect to('/')
+    end
+
+    get '/drawings/:id/edit' do
+      @models = Model.all
+      @drawing = Drawing[params[:id]]
+      erb :edit
     end
   end
 end
